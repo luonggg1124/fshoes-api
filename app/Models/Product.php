7c3e112564525e65dd0c8d9e76174297d418fff9
luonggg1124 <luongnm1124@gmail.com>
2024-08-30 22:29:46 +0700

    Create seeder category

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
     use HasFactory;
 
+    public function categories():BelongsToMany
+    {
+        return $this->belongsToMany(Category::class,'category_product','product_id','category_id');
+    }
 }
---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // php artisan db:seed --class=CategorySeeder
+        $categoryItems = ['Men','Women','Kids','Kids Shoes','Sale',"New & Featured"];
+        foreach ($categoryItems as $categoryItem) {
+            $slug = Str::slug($categoryItem).'-'.Str::random(5);
+            Category::query()->create([
+                'name' => $categoryItem,
+                'slug' => $slug,
+
+            ]);
+        }
+    }
+}
