a57e623b8c428c3fa836f0e67415efe58ad6a6d4
luonggg1124 <luongnm1124@gmail.com>
2024-08-30 19:57:25 +0700

    create migration model

---
+++ database/migrations/2024_08_30_071310_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained('categories');
+            $table->string('name');
+            $table->string('slug');
+            $table->decimal('price');
+            $table->decimal('sale_price');
+            $table->boolean('is_sale')->default(false);
+            $table->string('short_description');
+            $table->text('description');
+            $table->string('sku')->nullable();
+            $table->boolean('status')->default(1);
+            $table->integer('stock_qty')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/migrations/2024_08_30_071510_create_product_variations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_variations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products');
+            $table->string('sku')->nullable();
+            $table->decimal('price');
+            $table->decimal('sale_price');
+            $table->boolean('is_sale');
+            $table->integer('stock_qty');
+            $table->integer('stock_sold');
+            $table->text('image_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_variations');
+    }
+};
