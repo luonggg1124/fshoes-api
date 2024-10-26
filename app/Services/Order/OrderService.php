b5c6daef42b1bd72381d8b20cb3a46adf02ebbb8
vuhoanglong <longvulinhhoang@gmail.com>
2024-10-26 16:59:39 +0700

    Update api orders

--- app/Services/Order/OrderService.php
+++ app/Services/Order/OrderService.php
@@ -24,7 +24,7 @@ class OrderService implements OrderServiceInterface
 
     public function getAll($params): AnonymousResourceCollection
     {
-        $orders = $this->orderRepository->query()->with(['orderDetails' , 'orderHistory' ,'user' ]);
+        $orders = $this->orderRepository->query()->with(['orderDetails' , 'orderHistory' ,'user', 'orderDetails.product', 'orderDetails.variation' ]);
         if(isset($params['user_id'])){
             $orders->where('user_id' , $params['user_id']);
         }
@@ -36,7 +36,7 @@ class OrderService implements OrderServiceInterface
 
     public function findById(int|string $id)
     {
-        $cart = $this->orderRepository->query()->where('id', $id)->with(["orderDetails" , 'orderHistory' , 'user'])->first();
+        $cart = $this->orderRepository->query()->where('id', $id)->with(["orderDetails" , 'orderHistory' , 'user' , 'orderDetails.product', 'orderDetails.variation'])->first();
         if(!$cart){
             throw new ModelNotFoundException('Order not found');
         }
