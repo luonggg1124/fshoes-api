09279bd19b11150754777b23edc5f9a7696cab95
vuhoanglong <longvulinhhoang@gmail.com>
2024-11-10 21:43:14 +0700

    Update Statistic

--- app/Http/Controllers/Api/StatisticsController.php
+++ app/Http/Controllers/Api/StatisticsController.php
@@ -26,9 +26,27 @@ class StatisticsController extends Controller
 
     }
 
-    public function statistics()
+    public function statistics(Request $request)
     {
-
+        return response()->json([
+            "total_amount"=>$this->orderRepository->query()->sum('total_amount'),
+            "total_user"=>$this->userRepository->query()->count('id'),
+            "total_product"=>$this->productRepository->query()->count('id'),
+            "total_post"=>$this->postRepository->query()->count('id'),
+            "recent_order"=>$this->orderRepository->query()->orderBy('id' , 'DESC')->take(5)->get()->map(function($item){
+                    return[
+                        "user_id"=>$item->user->name,
+                        "total_amount"=>$item->total_amount,
+                    ];
+            }),
+            "recent_review"=>$this->reviewRepository->query()->orderBy('id' , 'DESC')->take(5)->get()->map(function($item){
+                return[
+                    "user_id"=>$item->user->name,
+                    "title"=>$item->title,
+                    "rating"=>$item->rating,
+                ];
+            }),
+        ],200);
     }
 
     public function order()
