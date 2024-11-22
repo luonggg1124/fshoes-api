ef0c38a956285d39d90902dc3caa13da420591aa
vuhoanglong <longvulinhhoang@gmail.com>
2024-11-22 14:58:16 +0700

    test import

--- app/Http/Controllers/Api/ImportVoucher.php
+++ app/Http/Controllers/Api/ImportVoucher.php
@@ -4,14 +4,27 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Imports\VoucherImport;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
+use Mockery\Exception;
 
 class ImportVoucher extends Controller
 {
     public function import(Request $request)
     {
-        Excel::import(new VoucherImport(), $request->file('vouchers'));
-        return redirect('/')->with('success', 'All good!');
+        try {
+            Excel::import(new VoucherImport(), $request->file('vouchers'));
+            return response()->json(["message"=>"Import voucher successfully"],200);
+        } catch (QueryException $exception) {
+            if ($exception->getCode() === '23000') {
+                return response()->json(['message' => "The voucher code already exists. Please choose a different value."], 422);
+            }
+            return response()->json(['message' => "Something went wrong. Please try again later."], 500);
+        } catch (Exception $exception) {
+            return response()->json("An unexpected error occurred. Please try again.", 500);
+        }
+
     }
 }
+
