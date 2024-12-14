06fb187d128d227fe764743b916ab45bcc14d068
luonggg1124 <luongnm1124@gmail.com>
2024-12-14 22:32:42 +0700

    fix

--- app/Models/ProductVariations.php
+++ app/Models/ProductVariations.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 
