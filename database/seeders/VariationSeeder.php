f4e00ae4f7d3c029cdc62646c00b17358096e40b
luonggg1124 <luongnm1124@gmail.com>
2024-12-17 17:54:50 +0700

    fake data

--- database/seeders/VariationSeeder.php
+++ database/seeders/VariationSeeder.php
@@ -42,55 +42,55 @@ class VariationSeeder extends Seeder
         ]);
         
 
-        // foreach (Product::query()->where('id','>',15)->take(15)->get() as $p) {
-        //     $attributes = [
-        //         'color' => Attribute::query()->find(1)->values()->take(2)->pluck('id'),
-        //         'size' => Attribute::query()->find(2)->values()->take(2)->pluck('id'),
-        //     ];
-        //     $result = [[]];
-        //     foreach ($attributes as $attribute => $values) {
-        //         $new = [];
-        //         foreach ($result as $variation){
-        //             foreach ($values as $value){
-        //                 $newVariation = $variation;
-        //                 $newVariation[$attribute] = $value;
-        //                 $new[] = $newVariation;
-        //             }
-        //         }
-        //         $result = $new;
-        //     }
-        //     foreach ($result as $var){
-        //         $variation = $p->variations()->create([
-        //             'sku' => $p->sku . '-' . Str::random(5),
-        //             'price' => $p->price + (random_int(1,5) / 10 * $p->price),
-        //             'import_price' => $p->price - (random_int(1,5) / 10 * $p->price),
-        //             'stock_qty' => random_int(20, 70),
-        //             'qty_sold' => random_int(20, 70),
-        //         ]);
+        foreach (Product::query()->where('id','>',15)->take(15)->get() as $p) {
+            $attributes = [
+                'color' => Attribute::query()->find(1)->values()->take(2)->pluck('id'),
+                'size' => Attribute::query()->find(2)->values()->take(2)->pluck('id'),
+            ];
+            $result = [[]];
+            foreach ($attributes as $attribute => $values) {
+                $new = [];
+                foreach ($result as $variation){
+                    foreach ($values as $value){
+                        $newVariation = $variation;
+                        $newVariation[$attribute] = $value;
+                        $new[] = $newVariation;
+                    }
+                }
+                $result = $new;
+            }
+            foreach ($result as $var){
+                $variation = $p->variations()->create([
+                    'sku' => $p->sku . '-' . Str::random(5),
+                    'price' => $p->price + (random_int(1,12) / 10 * $p->price),
+                    'import_price' => $p->price - (random_int(1,12) / 10 * $p->price),
+                    'stock_qty' => random_int(20, 70),
+                    'qty_sold' => random_int(20, 70),
+                ]);
 
 
-        //         $variation->values()->attach($var);
-        //         $values = $variation->values()->pluck('value');
-        //         $valueArr = [];
-        //         foreach ($values as $value) {
-        //             $v = Str::slug($value);
-        //             $valueArr[] = $v;
-        //         }
-        //         $valueStr = implode('-', $valueArr);
-        //         $slug = $valueStr . '.' . $variation->id;
-        //         $variation->name = $variation->product->name.' '.'['.implode(' - ',[...$values]).']';
-        //         $variation->classify = implode(' - ',[...$values]);
-        //         $variation->slug = $slug;
-        //         $variation->save();
-        //         $images = Image::factory(3)->create();
-        //         foreach ($images as $image) {
-        //             DB::table('product_variation_image')->insert([
-        //                 'product_variation_id' => $variation->id,
-        //                 'image_id' => $image->id,
-        //             ]);
-        //         }
-        //     }
-        // }
+                $variation->values()->attach($var);
+                $values = $variation->values()->pluck('value');
+                $valueArr = [];
+                foreach ($values as $value) {
+                    $v = Str::slug($value);
+                    $valueArr[] = $v;
+                }
+                $valueStr = implode('-', $valueArr);
+                $slug = $valueStr . '.' . $variation->id;
+                $variation->name = $variation->product->name.' '.'['.implode(' - ',[...$values]).']';
+                $variation->classify = implode(' - ',[...$values]);
+                $variation->slug = $slug;
+                $variation->save();
+                $images = $p->images;
+                foreach ($images as $image) {
+                    DB::table('product_variation_image')->insert([
+                        'product_variation_id' => $variation->id,
+                        'image_id' => $image->id,
+                    ]);
+                }
+            }
+        }
         // foreach (Product::query()->where('id','<','16')->take(15)->get() as $p) {
         //     $attributes = [
         //         'color' => Attribute::query()->find(1)->values()->where('id','>',2)->take(2)->pluck('id'),
