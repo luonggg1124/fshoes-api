7479952b560c6bcf9dc04ab250a4ed0386ee76c2
Vũ Linh Hoàng Long <141331311+vuhoanglong04@users.noreply.github.com>
2024-12-26 18:52:21 +0700

    Update OrderService.php

--- app/Services/Order/OrderService.php
+++ app/Services/Order/OrderService.php
@@ -41,12 +41,19 @@ class OrderService implements OrderServiceInterface
 
      public function getAll($params)
     {
-        $orders = $this->orderRepository->query()->with(['orderDetails', 'orderHistory', 'user' , 'user.image', 'orderDetails.variation', 'orderDetails.product'])
+        $orders = $this->orderRepository->query()
+                ->with(['orderDetails', 'orderHistory', 'user' , 'user.image',
+                  'orderDetails.variation',
+                  'orderDetails.product.images',
+                   'orderDetails.product',
+                   'orderDetails.product.images',
+                   'voucher', 
+                   ])
                   ->orderBy('created_at', 'desc');
         if (isset($params['user_id'])) {
             $orders->where('user_id', $params['user_id']);
         }
-         return $orders->paginate(10);
+        return $orders->paginate(10);
     }
 
     public function findById(int|string $id)
